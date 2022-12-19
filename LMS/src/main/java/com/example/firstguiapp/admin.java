762e81a5bf615156ea6632dfb383e75fd29c2360
Akmal <99531340+akmal-nuu@users.noreply.github.com>
2022-12-19 09:32:00 +0500

    Delete LMS directory

--- LMS/src/main/java/com/example/firstguiapp/admin.java
+++
@@ -1,5 +0,0 @@
-package com.example.firstguiapp;
-
-public class admin {
-
-}
--- LMS/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.firstguiapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.firstguiapp to javafx.fxml;
-    exports com.example.firstguiapp;
-}
