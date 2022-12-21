d15cb8df89739ff4301f371eb342ea10e55a8114
Farangiz Ismoilova <2110185@newuzbekistanuniversity.uz>
2022-12-21 14:55:22 +0500

    Notification

---
+++ LMS/src/main/java/com/example/firstguiapp/NotificationView.java
@@ -0,0 +1,21 @@
+package com.example.firstguiapp;
+
+import com.example.firstguiapp.HelloApplication;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class NotificationView extends Application {
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("NotificationView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 590, 400);
+        stage.setTitle("Timetable");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
