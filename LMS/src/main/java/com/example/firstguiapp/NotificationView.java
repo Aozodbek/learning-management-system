890b37ee72dd416a8671789977ba696941de613f
Farangiz Ismoilova <2110185@newuzbekistanuniversity.uz>
2022-12-21 15:20:00 +0500

    noti

--- LMS/src/main/java/com/example/firstguiapp/NotificationView.java
+++ LMS/src/main/java/com/example/firstguiapp/NotificationView.java
@@ -5,6 +5,9 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
