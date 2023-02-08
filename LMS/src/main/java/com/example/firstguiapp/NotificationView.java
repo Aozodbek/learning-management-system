4ccae07a781916d4f1fcc931acaf7141486f5424
2110185 <2110185@newuzbekistanuniversity.uz>
2023-02-08 13:18:09 +0500

    saaaaa

--- LMS/src/main/java/com/example/firstguiapp/NotificationView.java
+++ LMS/src/main/java/com/example/firstguiapp/NotificationView.java
@@ -7,8 +7,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.effect.ImageInput;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -17,20 +20,19 @@ import java.io.IOException;
 public class NotificationView extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("NotificationView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 590, 400);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("NotificationView");
         stage.setScene(scene);
         stage.show();
 
     }
-    public void showmessage (){
-        Alert notification = new Alert(Alert.AlertType.INFORMATION, "Don't touch,", ButtonType.CLOSE);
-        notification.show();
-    }
+
 
     public void btcnotification(ActionEvent actionEvent) {
-        Alert notification = new Alert(Alert.AlertType.INFORMATION, "Don't touch,", ButtonType.CLOSE);
-        notification.show();
+        Alert notification = new Alert(Alert.AlertType.INFORMATION, "Close the page!!!", ButtonType.CLOSE);
+        notification.setTitle("Wellcome to Student News");
+        notification.setHeaderText("Wellcome to Student News");
 
+        notification.show();
     }
 }
